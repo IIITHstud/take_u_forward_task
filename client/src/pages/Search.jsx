a7aed726ac0ff41f6356f745a9f23e2625c68e01
ChiragGoyal2222 <chirag.goyal@students.iiit.ac.in>
2024-08-12 00:46:29 +0530

    done

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -45,7 +45,7 @@ export default function Search() {
         const data = await res.json();
         setPosts(data.posts);
         setLoading(false);
-        if (data.posts.length === 15) {
+        if (data.posts.length === 9) {
           setShowMore(true);
         } else {
           setShowMore(false);
